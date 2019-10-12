9d8915c190d2aab0c399e995e728ea43afe4f74a
Piou-piou <pilloud.anthony@gmail.com>
2019-10-12 18:30:06 +0200

    [func] initialize bundle

---
+++ Controller/RibsCronController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PiouPiou\RibsCronBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class RibsCronController extends AbstractController
+{
+}
---
+++ RibsCronBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PiouPiou\RibsCronBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RibsCronBundle extends Bundle
+{
+}
